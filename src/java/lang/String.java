96be921866c619c12ed95953d58bd2c00209b452
Golde-nchow <570384231@qq.com>
2021-01-25 16:01:36 +0800

    String#charAt(int index)：获取字符串内对应索引的单个字符：根据自己理解进行翻译

--- src/java/lang/String.java
+++ src/java/lang/String.java
@@ -782,22 +782,24 @@ public final class String implements java.io.Serializable, Comparable<String>, C
     }
 
     /**
+     * 在指定的索引位置返回对应的字符。索引的范围为 0 至 length()-1。
+     * 第一个字符是在字符序列索引为 0 的位置，下一个字符就在索引 1 位，以此类推。
+     *
      * Returns the {@code char} value at the
      * specified index. An index ranges from {@code 0} to
      * {@code length() - 1}. The first {@code char} value of the sequence
      * is at index {@code 0}, the next at index {@code 1},
      * and so on, as for array indexing.
      *
+     * 若字符值对应的这个索引是一个 surrogate，那么该 surrogate 的值都会被返回。
+     *
      * <p>If the {@code char} value specified by the index is a
      * <a href="Character.html#unicode">surrogate</a>, the surrogate
      * value is returned.
      *
-     * @param      index   the index of the {@code char} value.
-     * @return     the {@code char} value at the specified index of this string.
-     *             The first {@code char} value is at index {@code 0}.
-     * @exception  IndexOutOfBoundsException  if the {@code index}
-     *             argument is negative or not less than the length of this
-     *             string.
+     * @param      index   索引下标
+     * @return     指定下标对应的字符。
+     * @exception  IndexOutOfBoundsException  若索引值不在字符串范围内
      */
     public char charAt(int index) {
         if ((index < 0) || (index >= value.length)) {
